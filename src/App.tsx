89dcf032f32fbafe41f60a41cd6051edc412b040
张为杰 <zhangwj0520@hotmail.com>
2020-03-22 21:58:09 +0800

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ConfigProvider, Button } from 'antd';
+import zhCN from 'antd/es/locale/zh_CN';
+import moment from 'moment';
+
+import Root from './routes/Root';
+
+moment.locale('zh-cn');
 
 function App() {
-  const get = (a: string) => {
-    return a;
-  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <button type="button">ddddd</button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          Edit
-          <code>src/App.tsx</code>
-          and save to reload.
-        </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 3
-        </a>
-      </header>
-    </div>
+    <ConfigProvider locale={zhCN}>
+      {/* <Root /> */}
+      dddd是
+    </ConfigProvider>
   );
 }
 
